efee5a129ad0776dd817c395c87642059c245bfc
Yu Sun <sunyu0410@gmail.com>
2024-08-30 17:11:54 +1000

    Sorted the files of the lung re-irr data

--- tools/pmcc_rmse.py
+++ tools/pmcc_rmse.py
@@ -1,12 +1,12 @@
 from pathlib import Path
-import pydicom as dicom
+import pydicom
 import SimpleITK as sitk
 import numpy as np
-
+import shutil
 
 # RTStruct
 def get_points(rs_file):
-    rs = dicom.read_file(rs_file)
+    rs = pydicom.read_file(rs_file)
     names = dict([(i.ROINumber, i.ROIName) for i in rs.StructureSetROISequence])
     pts = [
         (i.ReferencedROINumber, i.ContourSequence[0].ContourData)
@@ -21,7 +21,7 @@ def get_points(rs_file):
 
 class Mapper:
     """
-    Note that GetOrigin() will return opposite signs for the first two numbers
+    Note that GetOrigin() will return opposite signs for the first two numbers between Slicer and SITK
     https://discourse.slicer.org/t/discrepancy-in-image-origin-values-between-3d-slicer-and-simpleitk/35946/2
     https://slicer.readthedocs.io/en/latest/user_guide/coordinate_systems.html
     Slicer: RAS
@@ -61,31 +61,117 @@ class Mapper:
         return self.applyMatrix(self.matrix_inv, coord).astype(int)
 
 
-if __name__ == "__main__":
-    data_dir = Path(
-        r"\\petermac.org.au\shared\ImageStore\Reirradiation\Lung\PMCC_ReIrrad_14"
-    )
+def get_dcm_info(infile:str):
+    dcm = pydicom.read_file(infile)
+    return dcm.Modality, dcm.FrameOfReferenceUID
 
+
+def dcm2nii(fl:list, outfile:str):
+    img_reader = sitk.ImageSeriesReader()
+    img_reader.SetFileNames(fl)
+    img = img_reader.Execute()
+    sitk.WriteImage(img, outfile)
+
+
+def get_remain_files(data_dir:Path, filelists):
+    files = set([i.name for i in data_dir.iterdir()])
+    for fl in filelists:
+        fnames = set([Path(i).name for i in fl])
+        files = files - fnames
+    return [data_dir / f for f in files if f.endswith('.dcm')]
+
+
+def get_rtstruct_file(fnames:list, frameUID:str):
+    dcms = {f: pydicom.read_file(f) for f in fnames}
+    output = {
+        i: dcm
+        for i, dcm in dcms.items()
+        if dcm.Modality == "RTSTRUCT" and dcm.FrameOfReferenceUID == frameUID
+    }
+    # assert len(output) == 1
+    return list(output.keys())
+
+
+def sort_files(data_dir):
+    '''Scan a folder and returns
+    {seriesUID: (ct_files, rs_file)}
+    '''
+    data_dir = Path(data_dir)
     reader = sitk.ImageSeriesReader()
+    ids = reader.GetGDCMSeriesIDs(data_dir.as_posix())
 
-    # ids = reader.GetGDCMSeriesIDs(data_dir.as_posix())
-    # print(ids)
-    # # ('1.2.246.352.221.4988577021572189020.6098343126741388731',
-    # #  '1.2.246.352.221.5330343737108144152.7175712638069911460',
-    # #  '1.2.246.352.221.5655056896598563174.9427470545387792538')
+    filelists = {i: reader.GetGDCMSeriesFileNames(data_dir.as_posix(), i) for i in ids}
+    info = {
+        i: get_dcm_info(fl[0]) for i, fl in filelists.items()
+    }  # seriesUID: (Mod, FrameUID)
 
-    # mod = dicom.read_file(reader.GetGDCMSeriesFileNames(data_dir.as_posix(), ids[0])[0]).Modality
-    # print(mod)
+    print(info)
 
-    fl = reader.GetGDCMSeriesFileNames(
-        data_dir.as_posix(), "1.2.246.352.221.5655056896598563174.9427470545387792538"
-    )
-    reader.SetFileNames(fl)
-    image = reader.Execute()
-    mapper = Mapper(image)
+    remaining_files = get_remain_files(data_dir, filelists.values())
 
-    pts = get_points(data_dir / "RS.PMCC_ReIrrad_14.CT_LM_062017.dcm")
-    pts_ijk = [mapper.lps2ijk(*pt) for (num, label, pt) in pts]
-    print(pts)
+    rs_files = {
+        seriesUID: get_rtstruct_file(remaining_files, frameUID)
+        for seriesUID, (mod, frameUID) in info.items() if mod=='CT'
+    }
 
-    # Can now get the ijk for the reference image points
+    return {
+        seriesUID: (filelists[seriesUID], rs_files[seriesUID])
+        for seriesUID, (mod, frameUID) in info.items() if mod=='CT'
+    }
+
+def transfer_files(file_info, out_dir):
+    for i, (seriesUID, (ct_files, rs_files)) in enumerate(file_info.items(), start=1):
+        each_out_dir = out_dir / str(i)
+        each_out_dir.mkdir(exist_ok=True)
+        date = pydicom.read_file(ct_files[0]).SeriesDate
+        dcm2nii(ct_files, (each_out_dir/f'ct-{seriesUID}.nii.gz').as_posix())
+        for rs_file in rs_files: shutil.copy(rs_file, each_out_dir)
+
+        print(
+            f'{i}\n{seriesUID}\n{ct_files}\n{rs_files}\n\n', 
+            file=open(out_dir/'src.txt', 'a')
+        )
+
+if __name__ == "__main__":
+    data_dir = Path(r'\\petermac.org.au\shared\ImageStore\Reirradiation\Lung')
+    save_dir = Path(r'P:\yusun\self-supervised-anatomical-embedding-v2\data\lung')
+    for folder in data_dir.iterdir():
+        # Skip 300xxx folders
+        if folder.name.startswith('300'): continue
+        
+        each_save_dir = save_dir / folder.name
+        if not each_save_dir.exists(): each_save_dir.mkdir(exist_ok=True)
+
+        file_info = sort_files(folder)
+        transfer_files(file_info, each_save_dir)
+
+
+
+    # # Example
+    # data_dir = Path(
+    #     r"\\petermac.org.au\shared\ImageStore\Reirradiation\Lung\PMCC_ReIrrad_14"
+    # )
+
+    # reader = sitk.ImageSeriesReader()
+
+    # # ids = reader.GetGDCMSeriesIDs(data_dir.as_posix())
+    # # print(ids)
+    # # # ('1.2.246.352.221.4988577021572189020.6098343126741388731',
+    # # #  '1.2.246.352.221.5330343737108144152.7175712638069911460',
+    # # #  '1.2.246.352.221.5655056896598563174.9427470545387792538')
+
+    # # mod = pydicom.read_file(reader.GetGDCMSeriesFileNames(data_dir.as_posix(), ids[0])[0]).Modality
+    # # print(mod)
+
+    # fl = reader.GetGDCMSeriesFileNames(
+    #     data_dir.as_posix(), "1.2.246.352.221.5655056896598563174.9427470545387792538"
+    # )
+    # reader.SetFileNames(fl)
+    # image = reader.Execute()
+    # mapper = Mapper(image)
+
+    # pts = get_points(data_dir / "RS.PMCC_ReIrrad_14.CT_LM_062017.dcm")
+    # pts_ijk = [mapper.lps2ijk(*pt) for (num, label, pt) in pts]
+    # print(pts)
+
+    # # Can now get the ijk for the reference image points
